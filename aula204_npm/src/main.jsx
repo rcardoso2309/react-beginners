e23f524c68997eff6d332ade66e2d3f89c836208
Rafael Dantas <dantas85.ie@gmail.com>
2024-11-05 10:25:20 +0000

    React npm

---
+++ aula204_npm/src/main.jsx
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom/client'
+import React from 'react'
+
+function App() {
+  return <h1>Origamid</h1>
+}
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
