b23dd1e9f6024e8ec371419bf90572514afb8a4a
Rafael Dantas <dantas85.ie@gmail.com>
2024-11-08 12:47:04 +0000

    array1

---
+++ aula208_array/src/App.jsx
@@ -0,0 +1,35 @@
+import React, { useLayoutEffect } from 'react'
+
+// const App = () => {
+//   const filmes = ['Before Sunrise', 'Before sunset', 'Before Midnight'];
+//   return ( 
+//     <ul>
+//       {filmes.map((filme) => (
+//         <li key={filme}>{filme}</li>
+//       ))};
+//     </ul>
+//   );
+// };
+
+const App = () => {
+  const livros = [
+    { nome: 'A Game of Thrones', ano: 1996 },
+    { nome: 'A Clash of Kings', ano: 1998 },
+    { nome: 'A Storm of Swords', ano: 2000 },
+  ];
+
+  return (
+    <ul>
+      {livros
+        .filter((livro) => livro.ano >= 1998)
+        .map((livro) => (
+          <li key={livro.nome}>
+            {livro.nome}, {livro.ano}
+          </li>
+        ))}
+    </ul>
+  );
+};
+
+
+export default App;
